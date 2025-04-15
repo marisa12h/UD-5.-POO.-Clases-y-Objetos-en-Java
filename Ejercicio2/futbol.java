dd0878d355e4d67cd1baf776f9cc5a0ad023efdb
marisa12h <mesptorb@g.educaand.es>
2025-04-15 18:05:43 +0200

    Add files via upload

---
+++ Ejercicio2/futbol.java
@@ -0,0 +1,8 @@
+package Ejercicio2;
+
+public class futbol {
+    public static void main(String[] args) {
+        arbitros primero = new arbitros("Juan", "Torres", "España", 4, 10, 0, 4);
+        primero.imprimirDatos();
+    }
+}
